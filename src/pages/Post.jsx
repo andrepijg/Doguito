db35621cdf6dd5d509ba895db4cc8e2b9129676c
Andrea Jaramillo <andrepijg@gmail.com>
2023-09-05 15:17:12 -0500

    Proyecto Doguito

---
+++ src/pages/Post.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import "../assets/css/componentes/card.css"
+import { buscar } from "../api/api";
+
+
+
+const Post = ({ url }) => {
+    
+        const [post, setPost] = useState({})
+        const { id } = useParams()
+        console.log(id);
+
+        const navigate = useNavigate()
+        
+        useEffect(() => {
+            buscar(`/posts/${id}`, setPost).catch(()=> {
+                navigate("/not-found")
+                
+            })
+        }, [id])
+
+        return (
+            <main className="container flex flex--center">
+                <article className="card post">
+                    <h2 className="post-card__title">{post.title}</h2>
+                    <p className="text__card">{post.body}</p>
+                </article>
+            </main>
+        )
+}
+
+export default Post
